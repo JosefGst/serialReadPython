87fbf32946dded485d6f968c96cb7ccae2777e6e
JosefGst <61586098+JosefGst@users.noreply.github.com>
2020-11-24 11:27:05 +0800

    duration is in seconds

--- serialRead.py
+++ serialRead.py
@@ -2,20 +2,21 @@ import serial                   #to get serial data
 import matplotlib.pyplot as plt #for ploting
 import numpy as np              #for FFT
 from scipy.signal import find_peaks  #find peaks of FFT
-import time
+from datetime import datetime
+import os                       #to create a folder
+import pandas as pd             #create csv file
 
-
-
+SAVING = 1
 
 #open serial port
-ser = serial.Serial(port='COM7', baudrate=115200, bytesize=8, stopbits=1, parity='N')
+ser = serial.Serial(port='COM17', baudrate=115200, bytesize=8, stopbits=1, parity='N')
 
 #check serial connection
 if ser.is_open:
     print("connected to serial port")
 else: #if not connected, connect now
     print("is now connecting to serial port")
-    ser = serial.Serial(port='COM7', baudrate=115200, bytesize=8, stopbits=1, parity='N')
+    ser = serial.Serial(port='COM17', baudrate=115200, bytesize=8, stopbits=1, parity='N')
     print("is now connected to serial port")
    
 datalist = []
@@ -32,12 +33,12 @@ while True: #main infinit loop
     data = ser.readline() #waits until data is to read
     data = data.replace(' \r\n', '')
     if not ("this is the end" in data): #if "this is the end" has not reached, continue appending & reading the data
-        datalist.append(int(data))
+        datalist.append(float(data))
         counter += 1
         #print(data)                  
     else:     #if 'this is the end' messages arrives, do all the data processing                 
         hertz = datalist.pop()              #the last entry is the frequancy
-        duration = float(datalist.pop())    #the second last entry is the duration of all measurments
+        duration = float(datalist.pop())/1000    #the second last entry is the duration of all measurments
         nEntries = len(datalist) 
         for i in range(nEntries):  #calculate the time vector
             timeVal = duration * (i+1) / nEntries 
@@ -46,7 +47,7 @@ while True: #main infinit loop
         #FFT#################################################################
         fhat = np.fft.fft(datalist,nEntries)   #compute FFT
         PSD = fhat * np.conj(fhat) / nEntries
-        freq = 1000 / duration * np.arange(nEntries) #1000 because duration is in ms
+        freq = 1 / duration * np.arange(nEntries) #1000 because duration is in ms
         L = np.arange(1, np.floor(nEntries/2), dtype = 'int')
 
         #use PSD to filter out noise ########################################
@@ -63,11 +64,11 @@ while True: #main infinit loop
         
         fig,axs = plt.subplots(2, 1)
         plt.sca(axs[0])
-        plt.title("Time Domain\nDuration: " + str(duration) + " [ms], Frequancy: " + str(hertz) + " [Hz]")
-        plt.xlabel('Time [ms]')
+        plt.title("Time Domain\nDuration: " + str(duration) + " [s], Frequancy: " + str(hertz) + " [Hz]")
+        plt.xlabel('Time [s]')
         plt.ylabel('Acceleration [mg]')
         plt.plot(timelist, datalist, label = 'accel Z')
-        plt.plot(timelist, ffilt, label = 'filtered aZ')
+        #plt.plot(timelist, ffilt, label = 'filtered aZ')
         plt.legend()
         plt.grid()
 
@@ -76,18 +77,36 @@ while True: #main infinit loop
         plt.xlabel('Frequancy [Hz]')
         plt.ylabel('Power [g^2]?')
         plt.plot(freq[L], PSD[L], LineWidth = 3, label = 'PSD')
-        plt.plot(freq[L], PSDclean[L], label = 'filtered PSD')
+        #plt.plot(freq[L], PSDclean[L], label = 'filtered PSD')
         plt.plot(freq[peaks], PSDclean.real[peaks], "x" )
-        for x, y in zip(freq[peaks], PSDclean.real[peaks]): #plot the numbers to the peaks
+        for x, y in zip(freq[peaks], PSDclean.real[peaks]): #plot the nuvalues to the peaks
             plt.text(x, y, " %.2f Hz" % x)
         plt.legend()
         plt.grid()
         #plt.xscale("log")
         #plt.yscale("log")
-
         plt.tight_layout()  #prevents the axis label and the title of the subplots to overlap
-        plt.show()
+        
 
+        if SAVING is 1:    # save the plot
+            now = datetime.now()
+            directory_filename = "D:\\RA_PROJECT\\" + now.strftime('%Y_%m\\%d')
+            if not os.path.exists(directory_filename):
+                os.mkdir(directory_filename)  
+            dt_string = now.strftime("%H-%M-%S")        
+            plt.savefig(directory_filename + '\\plot-{}.png'.format(dt_string), dpi=300)
+            print('Plot file saved')
+            
+            #save Excel
+            df1 = pd.DataFrame({'datapoints [mg]': datalist, 'Time [s]': timelist})
+            df2 = pd.DataFrame({'PSD [Power]': PSD.real[L], 'Freq [Hz]': freq[L]})
+            #print (df1, df2)
+            with pd.ExcelWriter(directory_filename + '\\data-{}.xlsx'.format(dt_string)) as writer: #write each dataframe to another sheet
+                df1.to_excel (writer, sheet_name='Time Domain')
+                df2.to_excel (writer, sheet_name='PSD')
+            print('Excle file saved')
+                
+        plt.show()
         datalist = []
         timelist = []
         duration = 0.0
